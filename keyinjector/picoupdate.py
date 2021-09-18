2e080890232134cbb89291cfc4396ae383780f2f
REMC <wynothockey@gmail.com>
2021-09-17 18:53:16 -0700

    Update picoupdate.py

--- keyinjector/picoupdate.py
+++ keyinjector/picoupdate.py
@@ -10,7 +10,7 @@ try:
 except:
     updateversion = 'latest'
 if sys.platform == 'linux':
-    ver_file = open(f'/media/{getpass.getuser()}/CIRCUITPY/version.txt', 'w')
+    ver_file = open(f'/media/{getpass.getuser()}/CIRCUITPY/version.txt', 'rw')
     version = ver_file.read()
     while version < int(request.urlopen('re-mc.github.io/keyinjector/version.txt')):
         os.system(f'wget re-mc.github.io/keyinjector/updates/{version+1}.pyw')
