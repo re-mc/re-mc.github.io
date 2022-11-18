48f5de3869a12918e07c37f5e10960c839e0d1de
REMC <wynothockey@gmail.com>
2022-11-17 22:06:29 -0800

    Add files via upload

--- python_installer/main.py
+++ python_installer/main.py
@@ -2,13 +2,19 @@
 
 from atools import *
 
-menu_text = "Test Menu:\n A: Test Program\n('exit' to quit) "
+wget = aImport('wget')
+
+menu_text = "Test Menu:\n A: Test Program\n B: Test Download\n('exit' to quit) "
 
 def test():
     print("Test")
 
+def test_download():
+    wget.download("https://github.com/re-mc/re-mc.github.io/raw/main/testfolder/test.py")
+
 functions = {
-    'a': test
+    'a': test,
+    'b': test_download
 }
 
 menu = Menu(functions, menu_text, use_inputmsg = True)
