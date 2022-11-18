de59c9c9f84aad829658642844791c60fce0f860
REMC <wynothockey@gmail.com>
2022-11-17 21:53:04 -0800

    Add files via upload

--- python_installer/main.py
+++ python_installer/main.py
@@ -1,2 +1,14 @@
-print("Test")
-input()
+import atools
+
+menu_text = "Test Menu:\n A: Test Program\n('exit' to quit) "
+
+def test():
+    print("Test")
+
+functions = {
+    'a': test
+}
+
+menu = Menu(functions, menu_text, use_inputmsg = True)
+
+menu.loop()
