504ddd917f176cbb299466f8a5d96ddb27b2fba6
REMC <wynothockey@gmail.com>
2022-11-17 22:13:38 -0800

    Add files via upload

--- python_installer/main.py
+++ python_installer/main.py
@@ -4,7 +4,7 @@ from atools import *
 
 wget = aImport('wget')
 
-menu_text = "Test Menu:\n A: Test Program\n B: Test Download\n('exit' to quit) "
+menu_text = "Test Menu:\n A: Test Program\n B: Test Download\n C: Install Mod\n('exit' to quit) "
 
 def test():
     print("Test")
@@ -12,9 +12,18 @@ def test():
 def test_download():
     wget.download("https://github.com/re-mc/re-mc.github.io/raw/main/testfolder/test.py")
 
+def autoinstall(modurl):
+    modname = wget.download(modurl)
+    os.system(f'copy "{modname}" %appdata%\\.minecraft\\mods\\')
+
+def download_mod():
+    url = input("Enter the URL of the mod: ")
+    autoinstall(url)
+
 functions = {
     'a': test,
-    'b': test_download
+    'b': test_download,
+    'c': download_mod
 }
 
 menu = Menu(functions, menu_text, use_inputmsg = True)
