3ff83d7b394947344b8e15b8d1eea3922e1848ea
REMC <wynothockey@gmail.com>
2022-12-13 17:25:10 -0800

    Add files via upload

--- python_installer/main.py
+++ python_installer/main.py
@@ -1,10 +1,18 @@
-# yes
+"""
+Minecraft mod installer
+"""
 
 from atools import *
 
 wget = aImport('wget')
 
-menu_text = "Test Menu:\n A: Test Program\n B: Test Download\n C: Install Mod\n('exit' to quit) "
+menu_text = "Test Menu:\n A: Set Minecraft Folder\n B: Test Program\n C: Test Download\n D: Install Mod\n E: Remove All Mods\n('exit' to quit) "
+
+minecraft_dir = "%appdata%\\.minecraft"
+
+def set_folder():
+    minecraft_dir = input("Enter your .minecraft path (default is %appdata%\\.minecraft )\n ")
+
 
 def test():
     print("Test")
@@ -14,16 +22,26 @@ def test_download():
 
 def autoinstall(modurl):
     modname = wget.download(modurl)
-    os.system(f'copy "{modname}" %appdata%\\.minecraft\\mods\\')
+    os.system(f'move "{modname}" {minecraft_dir}\\mods\\')
 
 def download_mod():
-    url = input("Enter the URL of the mod: ")
+    url = input("Enter the URL of the mod:\n ")
     autoinstall(url)
 
+def download_file():
+    url = input("Enter the URL of the mod:\n ")
+    wget.download(modurl)
+
+
+def remove_mods():
+    os.system(f"remove {minecraft_dir}\\mods\\*")
+
 functions = {
-    'a': test,
-    'b': test_download,
-    'c': download_mod
+    'a': set_folder,
+    'b': test,
+    'c': test_download,
+    'd': download_mod,
+    'e': remove_mods
 }
 
 menu = Menu(functions, menu_text, use_inputmsg = True)
